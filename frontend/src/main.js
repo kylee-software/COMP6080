d63b95b10e675d4b24efdf20469e2f1914a6d629
Hayden Smith <mail@chambalam.com>
2020-09-08 23:09:33 +1000

    Ready for 21T3

---
+++ frontend/src/main.js
@@ -0,0 +1,5 @@
+const config = require('./config.json');
+// A helper you may want to use when uploading new images to the server.
+import { fileToDataUrl } from './helpers.js';
+
+console.log('Let\'s go!');
